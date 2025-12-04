f65cf38840137de0c5bcdb1189531c1ed52ed3f1
KhaledGhassan01 <khaledghassan355@gmail.com>
2025-12-04 08:41:50 +0200

    task1.cy.js

--- task1.cy.js
+++ task1.cy.js
@@ -1,26 +1,87 @@
-/// <reference types="cypress"/>
+/// <reference types="cypress" />
 
-describe('Registration Page - Selector Tests', () => {
-      beforeEach(() => {
+describe('Sign up Page - Conduit', () => {
+
+  it('Verify elements using alternative commands (no should)', () => {
+
     cy.visit('https://demo.productionready.io/#/register');
-  });
-  it('should select username input using data-testid attribute selector', () => {
-    cy.get('input[placeholder="Username"]').should('exist').type('UserKhaled');
-  });
-  it('should select email input using attribute selector', () => {
-    cy.get('input[type="email"]').should('exist').type('user123@test.com');
-  });
-  it('should select password input using :nth-of-type pseudo-class selector', () => {
-    cy.get('input[type="password"]').should('exist').type('User@123');
-  });
-  it('should select sign up button using button text selector', () => {
-    cy.contains('button', 'Sign up').should('exist').click();
-  });
-  it('should select checkbox for terms acceptance using adjacent sibling selector', () => {
-    cy.get('input[type="checkbox"]').should('exist').check({ force: true });
-  });
-  it('should select error message container using class selector', () => {
-    cy.get('.error-messages').should('exist');
+
+    cy.get('a.navbar-brand')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('conduit');
+      });
+
+    cy.contains('a.nav-link', 'Home')
+      .invoke('attr', 'href')
+      .then((href) => {
+        expect(href).to.equal('#/');
+      });
+
+    cy.get('a.nav-link[href="#/login"]')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('Sign in');
+      });
+
+    cy.get('a.nav-link[href="#/register"]')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('Sign up');
+      });
+
+    cy.contains('h1', 'Sign up')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('Sign up');
+      });
+
+    cy.contains('a', 'Have an account?')
+      .invoke('attr', 'href')
+      .then((href) => {
+        expect(href).to.equal('#/login');
+      });
+
+    cy.get('input[placeholder="Username"]')
+      .invoke('attr', 'type')
+      .then((type) => {
+        expect(type).to.equal('text');
+      })
+      .type('cypressUserX');
+
+    cy.get('input[placeholder^="Email"]')
+      .invoke('attr', 'type')
+      .then((type) => {
+        expect(type).to.equal('email');
+      })
+      .type('cy@test.com');
+
+    cy.get('input[placeholder$="Password"]')
+      .invoke('attr', 'type')
+      .then((type) => {
+        expect(type).to.equal('password');
+      })
+      .type('StrongPass123');
+
+    cy.get('button[type="submit"]')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('Sign up');
+      })
+      .click();
+
+    cy.get('a.logo-font')
+      .invoke('text')
+      .then((text) => {
+        expect(text.trim()).to.equal('conduit');
+      });
+
+    cy.get('span.attribution')
+      .invoke('text')
+      .then((text) => {
+        expect(text).to.contain('An interactive learning project');
+      });
+
   });
 
 });
